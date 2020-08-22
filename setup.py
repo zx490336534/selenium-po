e8864fa7e073c676713e1c29685c16e75f78dfd4
钟鑫 <490336534@qq.com>
2020-08-22 10:27:49 +0800

    上传打包内容

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/22 10:26 上午
+# @Author  : zhongxin
+# @Email   : 490336534@qq.com
+# @File    : setup.py
+
+from setuptools import setup
+
+setup(
+    name='selenium_po',
+    version='1.0.0',
+    description='',
+    author='zhong xin',
+    author_email='490336534@qq.com',
+    url='https://github.com/zx490336534/selenium-po',
+    packages=['selenium_po'],
+)
